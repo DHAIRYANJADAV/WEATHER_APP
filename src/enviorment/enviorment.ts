3cb78699f1ac2d06901e4bb9fbae2c7d5e65bd9f
DHAIRYANJADAV <dhairyanjadav21@gnu.ac.in>
2023-04-29 12:25:00 +0530

    Initial commit

---
+++ src/enviorment/enviorment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName : 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue : 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName : 'X-RapidAPI-Key',
+    XRapidAPIKeyHeadeValue : '43a2cc3273msh8e25ea0c02eb98ap1e7ea5jsnd1fb3aa5d639'
+};
